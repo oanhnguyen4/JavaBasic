602ff3e3a88fe29c702178c3bdf8dca951f76acf
oanhnguyen4 <oanhnt1997@gmail.com>
2025-03-12 00:15:53 +0700

    Initialize new project

---
+++ src/day1/DataTypesAndVariables.java
@@ -0,0 +1,15 @@
+package day1;
+import java.io.*;
+
+public class DataTypesAndVariables {
+      String a;
+      public static void printString() {
+    	  System.out.print("Hello Word");
+      }
+      public static void main(String[] args) {
+    	  printString1();
+      }
+      public static void printString1() {
+    	  System.out.print("++++++\r\n" + "@@@@@@\r\n" + "******\r\n" + "######");
+      }
+}
